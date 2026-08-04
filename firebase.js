243c8aeb2ea4bc5c4add3c3339ea3f78078f552b
kolyaboss211-tech <kolyaboss211@gmail.com>
2026-08-05 01:45:22 +0200

    Add Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
 
+const firebaseConfig = {
+  apiKey: "ВСТАВЬ_API_KEY",
+  authDomain: "ВСТАВЬ_AUTH_DOMAIN",
+  projectId: "ВСТАВЬ_PROJECT_ID",
+  storageBucket: "ВСТАВЬ_STORAGE_BUCKET",
+  messagingSenderId: "ВСТАВЬ_MESSAGING_SENDER_ID",
+  appId: "ВСТАВЬ_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
