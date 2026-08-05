ab95b5e9c550639ee00b837b59168993a871879a
kolyaboss211-tech <kolyaboss211@gmail.com>
2026-08-05 03:23:14 +0200

    Update Firebase configuration and database method

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
 
 const firebaseConfig = {
-  apiKey: "ВСТАВЬ_API_KEY",
-  authDomain: "ВСТАВЬ_AUTH_DOMAIN",
-  projectId: "ВСТАВЬ_PROJECT_ID",
-  storageBucket: "ВСТАВЬ_STORAGE_BUCKET",
-  messagingSenderId: "ВСТАВЬ_MESSAGING_SENDER_ID",
-  appId: "ВСТАВЬ_APP_ID"
+  apiKey: "AIzaSyAVsq99vOv_H3QxukyZBFEzb4zR1J5ha68",
+  authDomain: "chats-2e309.firebaseapp.com",
+  databaseURL: "https://chats-2e309-default-rtdb.firebaseio.com",
+  projectId: "chats-2e309",
+  storageBucket: "chats-2e309.firebasestorage.app",
+  messagingSenderId: "888879915328",
+  appId: "1:888879915328:web:ece055c1794243915c9aa5",
+  measurementId: "G-D80W4RDT4V"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
+const db = firebase.database();
